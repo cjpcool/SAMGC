c8076796bbb39a2f32306c83dc3945677a6a4ab0
Jianpeng Chen <37957296+cjpcool@users.noreply.github.com>
2023-11-14 18:14:26 +0800

    Add files via upload

--- test_dblp.sh
+++ test_dblp.sh
@@ -1,1 +1,1 @@
-python samgc.py --dataset 'dblp' --model_name 'samgc_dblp.pkl' --order 8 --hidden_dim 256 --latent_dim 64 --use_cuda True --cuda_device 0
+python samgc.py --dataset 'dblp' --model_name 'samgc_dblp.pkl' --order 16 --hidden_dim 256 --latent_dim 64 --use_cuda True --cuda_device 0
